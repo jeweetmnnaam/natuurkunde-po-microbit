b05c5c5d1649b8433f7bbdb5aa3f4637d2b3fa3c
web dev is geen programmeren <89776902+jeweetmnnaam@users.noreply.github.com>
2022-04-11 10:03:59 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let isSwitched = 0
+let doorgelaten = 0
+let weerstand = 0
+while (true) {
+    if (input.buttonIsPressed(Button.B) && isSwitched == 0) {
+        isSwitched = 1
+        weerstand = 3.2 - doorgelaten
+        basic.showString("" + convertToText(weerstand) + "V")
+    } else if (input.buttonIsPressed(Button.A) && isSwitched == 0) {
+        isSwitched = 1
+        basic.showString(convertToText(pins.analogReadPin(AnalogPin.P0)))
+    } else {
+        isSwitched = 0
+        doorgelaten = pins.analogReadPin(AnalogPin.P0) / 1024 * 3.2
+        basic.showString("" + convertToText(doorgelaten) + "V")
+    }
+    basic.pause(100)
+}
